767dbea36f21dac28a9e2d22bc2aa515c0e534be
Samuel Le <samuelle107@gmail.com>
2023-05-28 17:33:32 -0500

    added naive old solution

--- 70. Climbing Stairs/index.ts
+++ 70. Climbing Stairs/index.ts
@@ -1,3 +1,36 @@
+// Naive choose method (expensive because of factorial)
+// function factorial(n: number): number {
+//   if (n === 0 || n === 1) return 1;
+
+//   let fac = n;
+//   for (let i = n - 1; i >= 1; i -= 1) {
+//     fac = fac * i;
+//   }
+
+//   return fac;
+// }
+
+// function choose(n: number, r: number): number {
+//   return factorial(n) / (factorial(r) * factorial(n - r));
+// }
+
+// function getDistinctNumberOfSteps(n: number): number {
+//   const low = Math.ceil(n / 2);
+//   let start = n;
+//   let r = 0;
+
+//   let distinct = 0;
+
+//   while (start >= r) {
+//     distinct += choose(start, r);
+
+//     r += 1;
+//     start -= 1;
+//   }
+
+//   return distinct;
+// }
+
 function fib(n: number): number {
   let p1 = 1;
   let p2 = 2;
