362639d24deb2052ec95496425589277e843defa
Samuel Le <samuelle107@gmail.com>
2023-05-29 10:49:58 -0500

    re-order

--- 412. Fizz Buzz/index.ts
+++ 412. Fizz Buzz/index.ts
@@ -2,12 +2,12 @@ function fizzBuzz(n: number): string[] {
   const arr: string[] = [];
 
   for (let i = 1; i <= n; i += 1) {
-    if (i % 3 === 0) {
+    if (i % 15 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
       arr.push("Fizz");
     } else if (i % 5 === 0) {
       arr.push("Buzz");
-    } else if (i % 15 === 0) {
-      arr.push("FizzBuzz");
     } else {
       arr.push(String(i));
     }
