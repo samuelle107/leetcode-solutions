97a1c5e235bd0430e46d48145ba49f3fa2cf8371
Samuel Le <samuelle107@gmail.com>
2023-05-30 10:51:34 -0500

    init

---
+++ 938. Range Sum of BST/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Time - O(N)
+ * Space - O(N)
+ * @param root
+ * @param low
+ * @param high
+ * @returns
+ */
+function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
+  if (!root) return 0;
+
+  /**
+   * Val is the root's val if the root val is between the low and high range, inclusively.
+   * Otherwise, it is 0.
+   * */
+  const val = low <= root.val && root.val <= high ? root.val : 0;
+
+  return (
+    rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high) + val
+  );
+}
