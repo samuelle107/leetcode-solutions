e023c15cbf31210481e38f5bd89710dbf2443bc4
Samuel Le <samuelle107@gmail.com>
2023-05-29 19:18:29 -0500

    clean up

--- 200. Number of Islands/index.ts
+++ 200. Number of Islands/index.ts
@@ -1,64 +1,74 @@
-function getAdjCells(x: number, y: number, grid: ("1" | "0")[][]) {
-  const adjNodes: [number, number][] = [];
+type Grid = ("1" | "0")[][];
 
+/**
+ *
+ * @param x - i position of the grid
+ * @param y  - j position of the grid
+ * @param grid - The input grid
+ * @param visited - visited set
+ * @returns All valid adjacent cells (Does not include visited cells)
+ */
+function getAdjCells(x: number, y: number, grid: Grid, visited: Set<string>) {
   const l = grid[x - 1]?.[y];
   const r = grid[x + 1]?.[y];
   const u = grid[x]?.[y - 1];
   const d = grid[x]?.[y + 1];
 
-  if (l && l === "1") adjNodes.push([x - 1, y]);
-  if (r && r === "1") adjNodes.push([x + 1, y]);
-  if (u && u === "1") adjNodes.push([x, y - 1]);
-  if (d && d === "1") adjNodes.push([x, y + 1]);
+  const adjNodes: [number, number][] = [];
 
-  return adjNodes;
+  if (l === "1") adjNodes.push([x - 1, y]);
+  if (r === "1") adjNodes.push([x + 1, y]);
+  if (u === "1") adjNodes.push([x, y - 1]);
+  if (d === "1") adjNodes.push([x, y + 1]);
+
+  return adjNodes.filter(([x, y]) => !visited.has(`${x}.${y}`));
 }
 
-function numIslands(grid: ("1" | "0")[][]): number {
+function bfs(
+  i: number,
+  j: number,
+  q: [number, number][],
+  visited: Set<string>
+) {
+  const strNode = `${i}.${j}`;
+
+  q.push([i, j]);
+  visited.add(strNode);
+
+  while (q.length > 0) {
+    const coords = q.pop();
+    const [x, y] = coords!;
+    const adjNodes = getAdjCells(x, y, grid, visited);
+
+    adjNodes.forEach((node) => {
+      const [x, y] = node;
+      const strNode = `${x}.${y}`;
+
+      visited.add(strNode);
+      q.push([x, y]);
+    });
+  }
+}
+
+function numIslands(grid: Grid): number {
   const visited = new Set<string>();
   const q: [number, number][] = [];
   let islands = 0;
 
+  // Loop through every cell and conduct a BFS if the cell has not been discovered
+  // and is part of and island
   for (let i = 0; i < grid.length; i += 1) {
     for (let j = 0; j < grid[0].length; j += 1) {
       const strNode = `${i}.${j}`;
       const node = grid[i][j];
 
+      // No point in doing a bfs in the water, or if the island has already been discovered
       if (node === "0" || visited.has(strNode)) continue;
 
+      bfs(i, j, q, visited);
       islands += 1;
-
-      q.push([i, j]);
-      visited.add(strNode);
-
-      while (q.length > 0) {
-        const coords = q.pop();
-
-        const [x, y] = coords!;
-
-        const adjNodes = getAdjCells(x, y, grid);
-
-        adjNodes.forEach((node) => {
-          const [x, y] = node;
-          const strNode = `${x}.${y}`;
-
-          if (!visited.has(strNode)) {
-            visited.add(strNode);
-            q.push([x, y]);
-          }
-        });
-      }
     }
   }
 
   return islands;
 }
-
-const grid: ("1" | "0")[][] = [
-  ["1", "1", "0", "0", "0"],
-  ["1", "1", "0", "0", "0"],
-  ["0", "0", "1", "0", "0"],
-  ["0", "0", "0", "1", "1"],
-];
-
-console.log(numIslands(grid));
