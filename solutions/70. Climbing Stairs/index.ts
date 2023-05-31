1602ef48ae1b737f78e26576a277fc378c38fddb
Samuel Le <samuelle107@gmail.com>
2023-05-31 12:53:52 -0500

    updated comments

--- solutions/70. Climbing Stairs/index.ts
+++ solutions/70. Climbing Stairs/index.ts
@@ -7,7 +7,7 @@ const memo = new Map();
  * @param n
  * @returns n!
  */
-function factorial(n: number) {
+function factorial(n: number): number {
   if (n === 0 || n === 1) {
     return 1;
   }
@@ -31,37 +31,45 @@ function choose(n: number, r: number): number {
 // 4 choose 0 = 1
 // 1 + 1 + 1 + 1
 
-// 3 choose 1 = 1
+// 3 choose 1 = 3
 // 2 + 1 + 1
 // 1 + 2 + 1
 // 1 + 1 + 2
 
-// 2 choose 2 = 1
+// 2 choose 2 = 1 (Least amount of steps it can take)
 // 2 + 2
+
+// 3
+
+// 3 choose 0 = 1
+// 1 + 1 + 1
+
+// 2 Choose 1 = 2 (Least amount of steps it can take)
+// 2 + 1
+// 1 + 2
 
 /**
  * Intuition - When writing out the possible combinations, I found that the base case for every n was just taking n "1" steps.
  * For every "1" pair, it can be combined into a "2".
  * For every 2, you can move the 2 around to make a different path.
  * As a result, for every 2 you add, you can use the mathematical choose formula to calculate all the placements of 2s that you have
- * It can be stated as Σ C(n - r, r) where r starts at 0 and increases by until (n-r) = r
+ * It can be stated as Σ C(n - r, r) where r starts at 0 and increases by 1 until (n-r) < r
  * Using that formula at every iteration yields the number of paths that is possible using (n-r) "1"s and r "2"s.
  * Sum up every iteration yields the number of all possible paths
  * @param n
  * @returns
  */
 function climbStairs(n: number): number {
-  let start = n;
   let r = 0;
+  let numDistinctPaths = 0;
 
-  let distinct = 0;
-
-  while (start >= r) {
-    distinct += choose(start, r);
+  // Stop iterating when r is greater than (n - r)
+  while (n - r >= r) {
+    console.log(n - r, r);
+    numDistinctPaths += choose(n - r, r);
 
     r += 1;
-    start -= 1;
   }
 
-  return distinct;
+  return numDistinctPaths;
 }
