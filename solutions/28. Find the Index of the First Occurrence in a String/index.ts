9fd88e44b13d34f63fd057021774a6fbf09deb47
Samuel Le <samuelle107@gmail.com>
2023-05-31 12:16:23 -0500

    comments

--- solutions/28. Find the Index of the First Occurrence in a String/index.ts
+++ solutions/28. Find the Index of the First Occurrence in a String/index.ts
@@ -1,3 +1,7 @@
+// Inefficient
+// Time - O(N * M)
+// Space O(1)
+// KMP algorithm and Boyer Moore algorithm can be looked into
 function strStr(haystack: string, needle: string): number {
   for (let i = 0; i < haystack.length; i += 1) {
     const currWord = haystack.slice(i, i + needle.length);
