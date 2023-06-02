1ab9318838a968802d0faf86e6ab87b840279c79
Samuel Le <samuelle107@gmail.com>
2023-06-01 22:00:04 -0500

    leetcode needs to update their repos

--- solutions/347. Top K Frequent Elements/index.ts
+++ solutions/347. Top K Frequent Elements/index.ts
@@ -1,28 +1,50 @@
-/**
- * Time - O(N * log(N)) + O(K)
- * Space - O(N)
- * @param nums
- * @param k
- * @returns
- */
+// /**
+//  * Time - O(N * log(N)) + O(K)
+//  * Space - O(N)
+//  * @param nums
+//  * @param k
+//  * @returns
+//  */
+// function topKFrequent(nums: number[], k: number): number[] {
+//   const freq = new Map<number, number>();
+
+//   for (let i = 0; i < nums.length; i += 1) {
+//     const num = nums[i];
+
+//     freq.set(num, (freq.get(num) ?? 0) + 1);
+//   }
+
+//   return Array.from(freq)
+//     .sort((a, b) => {
+//       return b[1] - a[1];
+//     })
+//     .slice(0, k)
+//     .map((item) => item[0]);
+// }
+
+import { MaxPriorityQueue } from "@datastructures-js/priority-queue";
+
 function topKFrequent(nums: number[], k: number): number[] {
   const freq = new Map<number, number>();
 
   for (let i = 0; i < nums.length; i += 1) {
     const num = nums[i];
-
     freq.set(num, (freq.get(num) ?? 0) + 1);
   }
 
-  return Array.from(freq)
-    .sort((a, b) => {
-      return b[1] - a[1];
-    })
-    .slice(0, k)
-    .map((item) => item[0]);
+  const heap = new MaxPriorityQueue<number>((key) => freq.get(key)!);
+
+  freq.forEach((value, key) => {
+    heap.push(key);
+  });
+
+  console.log(heap.toArray());
+
+  const arr: number[] = [];
+
+  for (let i = 0; i < k; i += 1) {
+    arr.push(heap.pop());
+  }
+
+  return arr;
 }
-
-const nums = [1, 1, 1, 2, 2, 3],
-  k = 2;
-
-console.log(topKFrequent(nums, 2));
