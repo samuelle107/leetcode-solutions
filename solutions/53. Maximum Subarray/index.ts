9872e513395dff2b7be804b20abf9ac395be43b7
Samuel Le <samuelle107@gmail.com>
2023-06-05 22:16:19 -0500

    start at 0

--- solutions/53. Maximum Subarray/index.ts
+++ solutions/53. Maximum Subarray/index.ts
@@ -6,7 +6,7 @@
 function maxSubArray(nums: number[]): number {
   let currSum = 0;
   let max = nums[0];
-  for (let i = 1; i < nums.length; i += 1) {
+  for (let i = 0; i < nums.length; i += 1) {
     if (currSum < 0) {
       currSum = 0;
     }
