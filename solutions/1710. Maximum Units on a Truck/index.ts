90c451699fe8db651c2e786eaef75e82f5b5b89b
Samuel Le <samuelle107@gmail.com>
2023-06-06 16:57:05 -0500

    comments

--- solutions/1710. Maximum Units on a Truck/index.ts
+++ solutions/1710. Maximum Units on a Truck/index.ts
@@ -1,24 +1,39 @@
+/**
+ * Time - O(N * Log(N))
+ * Space - O(1)
+ * @param boxTypes
+ * @param truckSize
+ * @returns
+ */
 function maximumUnits(boxTypes: number[][], truckSize: number): number {
+  // Greedy. Try to fit the most amount of units into a box
   const sortedBoxTypes = boxTypes.sort((a, b) => {
     return b[1] - a[1];
   });
 
-  let truckCount = 0;
+  // Keeps track of how many boxes are being used
+  let boxCount = 0;
+  // Keeps track of how many units are on the trucks
   let unitCount = 0;
 
   for (let i = 0; i < sortedBoxTypes.length; i += 1) {
-    const [num, units] = sortedBoxTypes[i];
+    const [numOfBoxes, unitQtyPerBox] = sortedBoxTypes[i];
 
-    if (truckCount + num > truckSize) {
-      const diff = truckSize - truckCount;
+    if (boxCount + numOfBoxes > truckSize) {
+      // If the current set of boxes cannot fit on the truck,
+      // Try to fit the remaining space with a partial amount of boxes
 
-      truckCount += diff;
-      unitCount += diff * units;
+      const diff = truckSize - boxCount;
+
+      boxCount += diff;
+      unitCount += diff * unitQtyPerBox;
 
       return unitCount;
-    } else if (truckCount + num <= truckSize) {
-      truckCount += num;
-      unitCount += num * units;
+    } else if (boxCount + numOfBoxes <= truckSize) {
+      // Add all of the boxes and its units to the truck
+
+      boxCount += numOfBoxes;
+      unitCount += numOfBoxes * unitQtyPerBox;
     } else {
       return unitCount;
     }
