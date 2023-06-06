d7d77feeec9c78e12510ce0c2ef64b0d0d48e4e3
Samuel Le <samuelle107@gmail.com>
2023-06-05 22:06:20 -0500

    init

---
+++ solutions/53. Maximum Subarray/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Kadane's algorithm
+ * @param nums
+ * @returns
+ */
+function maxSubArray(nums: number[]): number {
+  let currSum = 0;
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i += 1) {
+    if (currSum < 0) {
+      currSum = 0;
+    }
+
+    currSum += nums[i];
+
+    max = Math.max(max, currSum);
+
+    console.log(i, currSum, max);
+  }
+
+  return max;
+}
