1f6b26e7153d191c7d43942f77d8e4f6d093af36
Samuel Le <samuelle107@gmail.com>
2023-06-06 08:14:05 -0500

    min q impl

---
+++ solutions/215. Kth Largest Element in an Array/index.ts
@@ -0,0 +1,24 @@
+import { MinPriorityQueue } from "@datastructures-js/priority-queue";
+
+/**
+ * Time - O(log(K))
+ * Space - O(K)
+ * @param nums
+ * @param k
+ * @returns
+ */
+function findKthLargest(nums: number[], k: number): number {
+  const q = new MinPriorityQueue<number>();
+
+  for (let i = 0; i < nums.length; i += 1) {
+    q.enqueue(nums[i]);
+
+    // This will m ake the q always have the top k elements in ascending order
+    if (q.size() > k) {
+      // Popping removes the smallest number (in a min priority queue)
+      q.pop();
+    }
+  }
+
+  return q.pop();
+}
